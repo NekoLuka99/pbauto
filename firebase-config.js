e90e602a936e035d9d3ccf86086f5b1e9f32eb29
NekoLuka99 <lukas.schroeter99@gmail.com>
2025-04-05 11:07:50 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-<script type="module">
+<script type="module"
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
   import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
